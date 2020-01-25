56ddf67dbae01b136b82f6240aa9f3184e955220
maanadev <maanafunedu@gmail.com>
2020-01-26 01:35:48 +0530

    Fix master password flag error flag

--- cmd/get.go
+++ cmd/get.go
@@ -37,7 +37,7 @@ var getCmd = &cobra.Command{
 		id := args[0]
 		mPassword, err := inputs.GetFlagStringVal(cmd, inputs.FlagMasterPassword)
 		if err != nil {
-			return errors.Wrapf(err, inputs.ErrMsgCannotGetFlag, mPassword)
+			return errors.Wrapf(err, inputs.ErrMsgCannotGetFlag, inputs.FlagMasterPassword)
 		}
 		if mPassword == "" {
 			mPassword, err = inputs.PromptForMPassword()
