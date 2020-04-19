91206eef5bb36c88cf429a6ebe97d50dd8be0864
maanadev <maanafunedu@gmail.com>
2020-04-19 12:58:36 +0530

    Add tag support for debian build script

--- resources/release-scripts/build-debian-package.sh
+++ resources/release-scripts/build-debian-package.sh
@@ -7,7 +7,7 @@ WORK_DIR=$(dirname "${BASH_SOURCE[0]}")
 INSTALLER_CLONE_DIR="$WORK_DIR/password-manager-installers"
 
 WITHOUT_V_VERSION="$(echo "$VERSION" | sed 's/^.//')"
-git clone https://github.com/ThilinaManamgoda/password-manager-installers.git "$INSTALLER_CLONE_DIR" -b "$(echo "$VERSION" | sed 's/.$/x/' | sed 's/^.//')"
+git clone https://github.com/ThilinaManamgoda/password-manager-installers.git "$INSTALLER_CLONE_DIR" -b ${WITHOUT_V_VERSION}
 cp target/linux/$VERSION/password-manager "$INSTALLER_CLONE_DIR"
 docker run -it -v "${PWD}/$INSTALLER_CLONE_DIR":/home/ubuntu maanadev/debian-tools-builder:1.0.0 ${WITHOUT_V_VERSION}
 cp "$INSTALLER_CLONE_DIR/password-manager_${WITHOUT_V_VERSION}_amd64.deb" target/linux/$VERSION/
